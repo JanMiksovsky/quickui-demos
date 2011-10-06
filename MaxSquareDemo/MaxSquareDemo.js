25fc92a983f926ae12336c7cad138589bb9e9bfa
Jan Miksovsky <jan@cozi.com>
2011-10-05 17:00:48 -0700

    Move MaxSquareDemo to demos project

---
+++ MaxSquareDemo/MaxSquareDemo.js
@@ -0,0 +1,112 @@
+//
+// DemoPage
+//
+DemoPage = Page.subclass( "DemoPage", function renderDemoPage() {
+	this.properties({
+		"fill": "true",
+		"content": [
+			" ",
+			this._define( "$photoCanvas", PhotoCanvas.create({
+				"id": "photoCanvas"
+			}) ),
+			" "
+		]
+	}, Page );
+});
+DemoPage.prototype.extend({
+    
+    initialize: function() {
+        var apiKey = "c3685bc8d8cefcc1d25949e4c528cbb0";
+        var method = "flickr.interestingness.getList";
+        var self = this;
+        this.getFlickrPhotos(apiKey, method, function(flickrPhotos) {
+            var photos = $.map(flickrPhotos, function(flickrPhoto) {
+                return {
+                    src: self.getFlickrImageSrc(flickrPhoto),
+                    href: self.getFlickrImageHref(flickrPhoto)
+                };
+            })
+            self.$photoCanvas().photos(photos);
+        });
+    },
+    
+    getFlickrPhotos: function(apiKey, method, callback) {
+        var baseUrl = "http://api.flickr.com/services/rest/";
+        var count = Page.urlParameters().count || 12;
+        var url = baseUrl + 
+                    "?api_key=" + apiKey +
+                    "&method=" + method +
+                    "&per_page=" + count +
+                    "&format=json" +
+                    "&nojsoncallback=1";
+        $.getJSON(url, function (data) {
+            callback(data.photos.photo);
+        });
+    },
+    
+    getFlickrImageSrc: function(flickrPhoto) {
+        return "http://farm" + flickrPhoto.farm +
+               ".static.flickr.com/" + flickrPhoto.server +
+               "/" + flickrPhoto.id +
+               "_" + flickrPhoto.secret +
+               ".jpg";
+    },
+    
+    getFlickrImageHref: function(flickrPhoto) {
+        return "http://flickr.com/photo.gne?id=" + flickrPhoto.id;
+    }
+});
+
+//
+// Photo
+//
+Photo = Control.subclass( "Photo", function renderPhoto() {
+	this.properties({
+		"content": [
+			" ",
+			this._define( "$photo", Control( "<img id=\"photo\" />" ) ),
+			" "
+		]
+	}, Control );
+});
+Photo.prototype.extend({
+    href: Control.property(),
+    src: Control.chain("$photo", "attr/src"),
+    initialize: function() {
+        var self = this;
+        this.$photo().click(function() {
+            window.location.href = self.href();
+        });
+    }
+});
+
+//
+// PhotoCanvas
+//
+PhotoCanvas = Layout.subclass( "PhotoCanvas", function renderPhotoCanvas() {
+	this.properties({
+
+	}, Layout );
+});
+PhotoCanvas.prototype.extend({
+
+    // An array of URLs pointing to images.
+    photos: Control.property(function(photos) {
+        var photoElements = $.map(photos, function(photo) {
+            return Photo.create({
+                src: photo.src,
+                href: photo.href
+            });
+        });
+        this
+            .content(photoElements)
+            .layout();
+    }),
+
+    layout: function() {
+        var $children = this.children();
+        var cellSize = maxSquare.optimalCellSize(this.width(), this.height(), $children.length);
+        $children.css(cellSize);
+    }    
+});
+
---
+++ MaxSquareDemo/readme.md
@@ -0,0 +1,5 @@
+This is a demo of the MaxSquare algorithm for maximizing photo thumbnail
+sizes. See the post at http://miksovsky.blogs.com/flowstate/2011/03/maxsquare.html
+for more information
+
+You can run the demo at http://quickui.org/demos/MaxSquareDemo.
