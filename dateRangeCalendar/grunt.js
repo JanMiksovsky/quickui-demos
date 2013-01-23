406c4030ad2c2649f27718fece8f8b43d9c7de40
Jan Miksovsky <jan@quickui.org>
2013-01-23 13:22:18 -0800

    Remove old header (accidentally cut and pasted).

--- dateRangeCalendar/grunt.js
+++ dateRangeCalendar/grunt.js
@@ -1,12 +1,3 @@
-/*
- * Grunt file to build the QuickUI Catalog.
- *
- * This has to encompass two separate build systems: one for new controls built
- * with CoffeeScript + LESS, and one for older controls built with QuickUI
- * Markup (http://github.com/JanMiksovsky/quickui-markup). Both types of
- * controls are built separate, and the output of both are then combined.
- */
-
 module.exports = function(grunt) {
 
     grunt.loadNpmTasks( "grunt-contrib-coffee" );
